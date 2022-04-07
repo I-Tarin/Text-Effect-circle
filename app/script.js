e22a24bbb0fb292406ac6195cce14c1ba8094a93
I-Tarin <90906743+I-Tarin@users.noreply.github.com>
2022-04-07 22:26:28 +0500

    Update script.js

--- app/script.js
+++ app/script.js
@@ -3,7 +3,7 @@ text.innerHTML = text.textContent.replace(/\S/g, "<span>$&</span>");
 
 const element = document.querySelectorAll('span');
 for (let i = 0; i < element.length; i++) {
-  element[i].style.transform = "rotate(" + i * 18 + "deg)"
+  element[i].style.transform = "rotate(" + i * 19 + "deg)"
 }
 
 document.addEventListener("mousemove", function (e) {
